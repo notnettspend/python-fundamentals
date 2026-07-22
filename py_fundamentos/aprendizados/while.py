0e0fd68febf87839fb00843a675f5295da6ebb15
notnettspend <andradedefariahiagomateus@gmail.com>
2026-07-22 18:28:29 -0300

    Add files via upload

---
+++ py_fundamentos/aprendizados/while.py
@@ -0,0 +1,29 @@
+contador = 1
+
+while contador <= 5:
+    print (contador)
+    contador += 1
+
+
+contador2 = 10
+
+while contador2 >= 0:
+    print (contador2)
+    contador2 -= 1 
+
+
+contador3 = 1
+
+while contador3 <=3:
+    nome = input("digite seu nome: ")
+    print(f"Ola {nome}")
+    contador3 += 1
+
+
+totalsoma = 0
+contador4 = 1
+while contador4 <=5:
+    numerosoma = int(input("digite um numero para somar: "))
+    totalsoma += numerosoma
+    contador4 += 1
+print(f"A soma dos números é: {totalsoma}")    
